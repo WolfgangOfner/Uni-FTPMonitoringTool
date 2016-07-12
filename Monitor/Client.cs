672fa9daa11f8fad7e444633bfff32d8ea862508
wolfgangofner <wolfgangofner@aon.at>
2016-07-12 15:57:02 +0200

    Commit

---
+++ Monitor/Client.cs
@@ -0,0 +1,65 @@
+﻿namespace Monitor
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading;
+
+    /// <summary>
+    /// Represents our Server class.
+    /// </summary>
+    public class Client
+    {
+        private readonly UdpClient udpClient;
+        IAsyncResult ar_ = null;
+        int port = 0;
+        Thread t = null;
+
+        public Client(IPAddress ipAddress, int portN)
+        {
+           udpClient = new UdpClient(portN);
+           port = portN;
+        }
+
+        internal void Start()
+        {
+            if (t != null)
+            {
+                throw new Exception("Already started, stop first");
+            }
+            Console.WriteLine("Started listening");
+            Console.WriteLine("To end program press e");
+         
+                StartListening();         
+        }
+
+        private void StartListening()
+        {
+           ar_ = udpClient.BeginReceive(Receive, new object()); 
+        }
+
+        private void Receive(IAsyncResult ar)
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, port);
+            byte[] bytes = udpClient.EndReceive(ar, ref ip);
+            string message = Encoding.ASCII.GetString(bytes);
+            Console.WriteLine(message);
+            StartListening();
+        }
+
+        public void EndProgram()
+        {
+            try
+            {
+                udpClient.Close();
+                Console.WriteLine("Stopped listening");
+                Environment.Exit(0);
+            }
+            catch 
+            { 
+                // dont care if exception
+            }
+        }
+    }
+}
---
+++ Monitor/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = null;
+            int port = 0;
+            ConsoleKeyInfo consoleKeyInfo;
+
+            // Let the user input the IP-Address.
+            do
+            {
+                Console.Write("Bitte geben Sie die IP Adresse an: ");
+                try
+                {
+                    ipAddress = IPAddress.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong ip entered! Repeat input.\n");
+                    Console.ResetColor();
+                }
+            } while (ipAddress == null);
+
+            // Let the user input the port number.
+            do
+            {
+                Console.Write("Bitte geben Sie den Port an: ");
+                try
+                {
+                    port = int.Parse(Console.ReadLine());
+
+                    if (port < 1 || port > 65535)
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch (Exception)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong port entered! Port must be between 1 and 65535. Repeat input.\n");
+                    port = 0;
+                    Console.ResetColor();
+                }
+            } while (port == 0);
+           
+            Client client = new Client(ipAddress, port);
+            client.Start();
+
+            // run programm till its exited with e input
+            do
+            {
+                if (Console.KeyAvailable)
+                {
+                    consoleKeyInfo = Console.ReadKey(true);
+
+                    switch (consoleKeyInfo.KeyChar)
+                    {                       
+                        case 'e':
+                            client.EndProgram();
+                            break;
+                    }
+                }
+                Thread.Sleep(10);
+
+            } while (true);
+        }
+    }
+}
